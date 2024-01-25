cfb0290ba371da5b0862f5f85721099209762582
Ismail Korir <ismail.korir1997@gmail.com>
2024-01-25 23:39:36 +0100

    First commit

---
+++ src/Components/Navbar/Navbar.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import './Navbar.css' 
+import logo from '../Assets/logo.png'
+import cart_icon from '../Assets/cart_icon.png'
+const Navbar = () => {
+  return (
+    <div className= 'Navbar'>
+      <div className="nav-logo">
+        <img src={logo} alt="" />
+        <p>SHOPPER</p>
+      </div>
+      <ul className="nav-menu">
+        <li>Shop</li>
+        <li>Men</li>
+        <li>Women</li>
+        <li>Kids</li>
+      </ul>
+    </div>
+  )
+}
+
+export default Navbar
