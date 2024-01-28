a12f9f7ae2819ca6ff264356fa85c3488d642b33
Ismail Korir <ismail.korir1997@gmail.com>
2024-01-28 23:24:58 +0100

    Adding Products

--- src/Pages/Product.jsx
+++ src/Pages/Product.jsx
@@ -2,7 +2,8 @@ import React, { useContext } from 'react'
 import { ShopContext } from '../Context/ShopContext'
 import { useParams } from 'react-router-dom'
 import Breadcrum from '../Components/Breadcrums/Breadcrum';
-import Productdisplay from '../Components/ProductDisplay/ProductDisplay'
+import ProductDisplay from '../Components/ProductDisplay/ProductDisplay'
+import DescriptionBox from '../Components/DescriptionBox/DescriptionBox'
 
 const Product = () => {
   const {all_product}= useContext(ShopContext);
@@ -11,7 +12,8 @@ const Product = () => {
   return (
     <div>
       <Breadcrum product={product}/>
-      <Productdisplay product={product}/>
+      <ProductDisplay product={product}/>
+      <DescriptionBox />
     </div>
   )
 }
